22babe394b42a933945e25504f0762428cd03ee8
Vyacheslav Serdyuk <Navarachkou@gmail.com>
2022-05-05 17:21:59 +0500

    feat: implement base config

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line
+import css from './styles/style.css';
+
+window.addEventListener('load', console.log('hello'));
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const mode = process.env.NODE_ENV;
+const isDev = mode === 'development';
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: mode,
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: 'index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['eslint-loader']
+            }
+        ],
+    },
+    devServer: {
+        static: './dist',
+        port: 3000,
+        hot: isDev,
+    },
+    devtool: isDev ? 'source-map' : '',
+};
